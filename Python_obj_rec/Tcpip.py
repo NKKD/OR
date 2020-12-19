5a29719efc26618a7b8e1b9f40889947c0a1fab7
JNN <jniu098@gmail.com>
2020-12-19 16:40:36 +1300

    establish tcpip communication

--- Python_obj_rec/Tcpip.py
+++ Python_obj_rec/Tcpip.py
@@ -1,7 +1,7 @@
 import socket
 
 # define socket category and socket type in our case using TCP/IP
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # create a socket with IP address 192.168.12.248 port number 1025
 
@@ -10,7 +10,30 @@ Tcp_Port = 1025
 
 # Open the socket and listen
 
-serversocket.bind(Tcp_IP,Tcp_Port)
-serversocket.listen()
+s.bind((Tcp_IP,Tcp_Port))
+s.listen(1)
+
+conn,addr = s.accept()
+print ('Connection address:',addr)
+
+while True:
+
+    data = conn.recv(1024)
+    print ("recive data: ")
+    print(data)
+
+    if not data:
+        break
+        print("Connection lost")
+
+    if data == b'1':
+        print("Connection esitablished")
+
+
+
+
+
+conn.close()
+
 
 
