01a9f763f762a667d3b59bb028c624b735cf49c2
JNN <jniu098@gmail.com>
2021-02-09 12:31:38 +1300

    Object rotation (euler angle)

--- Python_SURF/SURF_Object_detection.py
+++ Python_SURF/SURF_Object_detection.py
@@ -1,13 +1,14 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy
 
 
 if __name__ == '__main__':
 
     MIN_MATCH_COUNT = 10
-    img1 = cv2.imread('target.jpg', 0)  # Target Image
-    img2 = cv2.imread('scene.jpg', 0)  # Scene Image
+    img1 = cv2.imread('e.jpg', 0)  # Target Image
+    img2 = cv2.imread('e2.jpg', 0)  # Scene Image
     # Initiate SIFT detector
     sift = cv2.xfeatures2d.SURF_create()
     # find the keypoints and descriptors with SIFT
@@ -45,15 +46,14 @@ if __name__ == '__main__':
                        flags=2)
     img3 = cv2.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
 
-    print('Transformation matrix is:', '\n', M, '\n')
-    print('Bounding box corners coordinates', '\n', dst, '\n')
+    print('Transformation matrix M is:', '\n', M, '\n')
+    print('Bounding box corners coordinates dst: ', '\n', dst, '\n')
     # print('centroid of the bounding box is', , '\n')
-
+    print(type(M))
 
 
     x0 = dst[0][0][0]
     y0 = dst[0][0][1]
-
 
     x1 = dst[1][0][0]
     y1 = dst[1][0][1]
@@ -63,16 +63,22 @@ if __name__ == '__main__':
 
     x3 = dst[3][0][0]
     y3 = dst[3][0][1]
-    #
-    # print(x0,y0,x1,y1,x2,y2,x3,y3)
 
-    xcenter = 0.5*(x3-x1) + x0
-    ycenter = 0.5*(y1-y0) + y0
+    x = [x1,x2,x3,x0]
+    y = [y0,y1,y2,y3]
 
-    print(xcenter,ycenter)
+    print("Max value element : ", max(x))
+    print("Max value element : ", max(y))
+    print("Max value element : ", min(x))
+    print("Max value element : ", min(y))
+
+
+    xcenter = 0.5*(max(x)-min(x)) + min(x)
+    ycenter = 0.5*(max(y)-min(y)) + min(y)
+
+    print("the center is x y: ",xcenter,ycenter)
 
     plt.imshow(img2), plt.show()
-
 
 
 
